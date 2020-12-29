3a8b5fdca4a6399bbd9a3349ff9e9d6e18a394c4
michaelrambeau <michael@medmain.com>
2020-12-29 11:39:08 +0900

    Style adjustments, remove usused components

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -1,13 +1,7 @@
 import React from "react";
-// import svg from './star.svg'
 import formatDelta from "../utils/formatDelta";
 
 const Stars = ({ value, decimals }) => {
-  return (
-    <span className="stars">
-      +{formatDelta(value, decimals)}☆
-      {/* <img className="star" src={svg} alt="☆" width="14" height="16" /> */}
-    </span>
-  );
+  return <span className="stars">+{formatDelta(value, decimals)}☆</span>;
 };
 export default Stars;
